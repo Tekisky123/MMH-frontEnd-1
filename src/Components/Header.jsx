fa7198c6d30c7a54e91357fcd738a641dc4130b0
Syed Abbubakar <syed8040@gmail.com>
2023-12-13 20:00:19 +0530

    RegisteredPatients updated

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,72 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "../Assets/Styles/Header.css";
+import Home from "./Pages/Home";
+// import Dashboard from "./Pages/Dashboard";
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  const [isOpen, setIsOpen] = useState(false);
+  const [activeTab, setActiveTab] = useState("home");
 
-export default Header
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
+    setIsOpen(false); // Close the mobile menu after clicking a tab
+  };
+  return (
+    <nav className={`navbar ${isOpen ? "open" : ""}`}>
+      <div className="logo"></div>
+      <ul className={`nav-list ${isOpen ? "open" : ""}`}>
+        <li
+          className={activeTab === "home" ? "active" : ""}
+          style={{
+            color: activeTab === "home" ? "#4caf50" : "inherit",
+            textDecoration: activeTab === "home" ? "underline" : "none",
+          }}
+        >
+          <Link onClick={() => handleTabClick("home")} to="/home">
+            Home
+          </Link>
+        </li>
+        <li className={activeTab === "yojna" ? "active" : ""}>
+          <Link onClick={() => handleTabClick("yojna")} href="#yojna">
+            Yojna Details
+          </Link>
+        </li>
+        <li className={activeTab === "new-patient" ? "active" : ""}>
+          <Link
+            onClick={() => handleTabClick("new-patient")}
+            href="#new-patient"
+          >
+            New Patient
+          </Link>
+        </li>
+        <li className={activeTab === "register-patient" ? "active" : ""}>
+          <Link
+            onClick={() => handleTabClick("register-patient")}
+            to="/register"
+          >
+            Registered Patients
+          </Link>
+        </li>
+        <li className={activeTab === "user" ? "active" : ""}>
+          <Link onClick={() => handleTabClick("user")} href="#user">
+            User
+          </Link>
+        </li>
+        <li className={activeTab === "dashboard" ? "active" : ""}>
+          <Link onClick={() => handleTabClick("dashboard")} to="/dashboard">
+            Dashboard
+          </Link>
+        </li>
+      </ul>
+      <div className="burger-menu" onClick={handleToggle}>
+        &#9776;
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
--- src/Components/Pages/RegisteredPatients.jsx
+++ src/Components/Pages/RegisteredPatients.jsx
@@ -1,25 +1,36 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 // import axios from 'axios';
 import "../../Assets/Styles/RegisteredPatients.css";
 import check from "../../Assets/Images/check.png";
 import error from "../../Assets/Images/error.png";
+import axios from "axios";
 
-const RegisterPatient = () => {
-  
+const RegisteredPatients = () => {
+  const [file, setFile] = useState(null);
+  const [data, setData] = useState([]);
+
+  // const dataArray = Object.values(data);
+
+  //   const [loader, setLoader] = useState(false);
+
+  const baseURL = "http://13.126.14.109:5050/patient/getpatient";
+
+  useEffect(() => {
+    axios
+      .get("http://13.126.14.109:5050/patient/getpatient")
+      .then((responce) => {
+        console.log(responce.data.result);
+
+        setData(responce.data.result);
+      });
+
+    // getData();
+  }, []);
+
   const [showDetails, setShowDetails] = useState(false);
 
-  const handleFileChange = (e) => {
-    const selectedFile = e.target.files[0];
-
-    // Perform logic to handle the file, for example, you can upload it to a server
-    // Simulating asynchronous file upload using setTimeout
-    
-
-  
-
- 
-
-  const handleShowDetails = () => {
+  const handleShowDetails = (id) => {
+    console.log("index",id);
     setShowDetails(!showDetails);
   };
 
@@ -28,89 +39,95 @@ const RegisterPatient = () => {
   };
 
   return (
-    <div className="patient-card">
-      <div className="patient-data">
-        <h6>ID: </h6>
-        <h6>Name: </h6>
-        <h6>Gender: </h6>
-        <h6>Age: </h6>
-        {/* Add other details as needed */}
-      </div>
-      <div className="data-btn">
-        
-        <button className="btn-register-more" onClick={handleShowDetails}>
-          More Info
-        </button>
-      </div>
+    <>
+      {data.map((item, index) => {
+        return (
+          <div className="patient-card">
+            <div className="patient-data">
+              <h6>ID: {index + 1} </h6>
+              <h6>Name: {item.patientDetails.name} </h6>
+              <h6>Gender: {item.patientDetails.sex}</h6>
+              <h6>Age: {item.patientDetails.age} </h6>
+              {/* Add other details as needed */}
+            </div>
+            <div className="data-btn">
+              <button className="btn-register-more" onClick={(_id)=>handleShowDetails(_id)}>
+                More Info
+              </button>
+            </div>
 
-      {showDetails && (
-        <div className="patient-details-sidebar">
-          <span className="close-icon" onClick={handleSidebarClose}>
-            ❌
-          </span>
-          <h2>Patient Details</h2>
-          <table>
-            <thead>
-              <tr>
-                <th>Attribute</th>
-                <th>Value</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>ID</td>
-                <td></td>
-              </tr>
-              <tr>
-                <td>Name</td>
-                <td></td>
-              </tr>
-              <tr>
-                <td>Gender</td>
-                <td></td>
-              </tr>
-              {/* <tr>
+            {showDetails && (
+              <div className="patient-details-sidebar">
+                <span className="close-icon" onClick={handleSidebarClose}>
+                  ❌
+                </span>
+                <h2>Patient Details</h2>
+                <table>
+                  <thead>
+                    <tr>
+                      <th>Attribute</th>
+                      <th>Value</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>ID</td>
+                      <td>{index + 1}</td>
+                    </tr>
+                    <tr>
+                      <td>Name</td>
+                      <td>{item.patientDetails.name}</td>
+                    </tr>
+                    <tr>
+                      <td>Gender</td>
+                      <td>{item.patientDetails.sex}</td>
+                    </tr>
+                    {/* <tr>
                 <td>Age</td>
                 <td>{patient.age}</td>
               </tr> */}
-              <tr>
-                <td>Age</td>
-                <td></td>
-              </tr>
-              {/* <tr>
-                <td>Document</td>
-                <td>
-                  {file ? (
-                    <>
-                      <img
-                        src={check}
-                        alt="File Uploaded"
-                        className="file-upload-logo"
-                      />
-                      <span className="file-upload">File uploaded</span>
-                    </>
-                  ) : (
-                    <>
-                      <img
-                        src={error}
-                        alt="File Not Uploaded"
-                        className="file-upload-logo"
-                      />
-                      <span className="file-not-found">File not uploaded</span>
-                    </>
-                  )}
-                </td>
-              </tr> */}
-              {/* Add other rows for additional details */}
-            </tbody>
-          </table>
-          {/* Fetch additional patient details using Axios or your preferred method */}
-          {/* Display additional details here */}
-        </div>
-      )}
-    </div>
+                    <tr>
+                      <td>Age</td>
+                      <td>{item.patientDetails.age}</td>
+                    </tr>
+                    <tr>
+                      <td>Document</td>
+                      <td>
+                        {file ? (
+                          <>
+                            <img
+                              src={check}
+                              alt="File Uploaded"
+                              className="file-upload-logo"
+                            />
+                            <span className="file-upload">File uploaded</span>
+                          </>
+                        ) : (
+                          <>
+                            <img
+                              src={error}
+                              alt="File Not Uploaded"
+                              className="file-upload-logo"
+                            />
+                            <span className="file-not-found">
+                              File not uploaded
+                            </span>
+                          </>
+                        )}
+                      </td>
+                    </tr>
+                    {/* Add other rows for additional details */}
+                  </tbody>
+                </table>
+                {/* Fetch additional patient details using Axios or your preferred method */}
+                {/* Display additional details here */}
+              </div>
+            )}
+          </div>
+        );
+      })}
+    </>
   );
 };
-}
 
-export default RegisterPatient;
+export default RegisteredPatients;
