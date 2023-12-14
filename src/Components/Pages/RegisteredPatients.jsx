0d14131ee4aa016dcea1e7a761d195ffd048b5f9
Syed Abbubakar <syed8040@gmail.com>
2023-12-14 17:37:09 +0530

    RegisteredPatients updated on Thursday 14/12/2023

--- src/Components/Pages/RegisteredPatients.jsx
+++ src/Components/Pages/RegisteredPatients.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react";
-// import axios from 'axios';
+import React, { useState, useEffect } from "react";
 import "../../Assets/Styles/RegisteredPatients.css";
 import check from "../../Assets/Images/check.png";
 import error from "../../Assets/Images/error.png";
@@ -7,61 +6,249 @@ import axios from "axios";
 
 const RegisteredPatients = () => {
   const [file, setFile] = useState(null);
+  // const [showDetails, setShowDetails] = useState(false);
+  const [showStatus, setShowStatus] = useState(false);
   const [data, setData] = useState([]);
+  const [activePatientId, setActivePatientId] = useState(false);
 
-  // const dataArray = Object.values(data);
-
-  //   const [loader, setLoader] = useState(false);
-
-  const baseURL = "http://13.126.14.109:5050/patient/getpatient";
+  const baseURL = "http://13.126.14.109:4000/patient/getpatient";
 
   useEffect(() => {
-    axios
-      .get("http://13.126.14.109:5050/patient/getpatient")
-      .then((responce) => {
-        console.log(responce.data.result);
+    axios.get(baseURL).then((responce) => {
+      console.log(responce.data.result);
 
-        setData(responce.data.result);
-      });
+      setData(responce.data.result);
+    });
 
     // getData();
   }, []);
 
-  const [showDetails, setShowDetails] = useState(false);
+  const handleShowDetails = (_id) => {
+    // setShowDetails(!showDetails);
+    console.log("index", _id);
+    setShowStatus(false);
+    setActivePatientId(_id);
+    
+  };
 
-  const handleShowDetails = (id) => {
-    console.log("index",id);
-    setShowDetails(!showDetails);
+  const handleShowStatus = () => {
+    console.log("Show Status clicked");
+    setShowStatus(!showStatus);
+    // setShowDetails(false);
+    setActivePatientId(null);
   };
 
   const handleSidebarClose = () => {
-    setShowDetails(false);
+    // setShowDetails(false);
+    setShowStatus(false);
+    setActivePatientId(!false);
   };
 
   return (
     <>
-      {data.map((item, index) => {
+     <div className="maincontainer">
+     {data.map((item, index) => {
+      const isDetailsActive = activePatientId === item.patientDetails._id;
         return (
-          <div className="patient-card">
+          <div className="patient-card" key={index}>
             <div className="patient-data">
-              <h6>ID: {index + 1} </h6>
+              <h6>ID: {item.patientDetails._id}</h6>
               <h6>Name: {item.patientDetails.name} </h6>
-              <h6>Gender: {item.patientDetails.sex}</h6>
+              <h6>Gender: {item.patientDetails.sex} </h6>
               <h6>Age: {item.patientDetails.age} </h6>
-              {/* Add other details as needed */}
-            </div>
-            <div className="data-btn">
-              <button className="btn-register-more" onClick={(_id)=>handleShowDetails(_id)}>
+
+              <p>
+             
+                {file ? (
+                  <div className="file-upload-or-not">
+                    <img
+                      src={check}
+                      alt="File Uploaded"
+                      className="file-upload-logo"
+                    />
+                    <span className="file-upload">File uploaded</span>
+                  </div>
+                ) : (
+                  <div className="file-upload-or-not">
+                    <img
+                      src={error}
+                      alt="File Not Uploaded"
+                      className="file-upload-logo"
+                    />
+                    <span className="file-not-found">File not uploaded</span>
+                    <div className="data-btn">
+              <button className="btn-register-more" onClick={()=>handleShowDetails(item.patientDetails._id)}>
                 More Info
               </button>
+              <button
+                className="btn-register-status btn-register-status"
+                onClick={handleShowStatus}
+              >
+                Status
+              </button>
             </div>
-
-            {showDetails && (
+                    <br />
+                   
+                  </div>
+                  
+                )}
+              </p>
+              
+            </div>
+            
+            {isDetailsActive && (
               <div className="patient-details-sidebar">
                 <span className="close-icon" onClick={handleSidebarClose}>
                   ❌
                 </span>
-                <h2>Patient Details</h2>
+                <h1 className="table-heading">Patient Details</h1>
+                <table>
+                  <thead>
+                    <tr>
+                      <th>Attribute</th>
+                      <th>Value</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>Patient Name</td>
+                      <td>{item.patientDetails.name} </td>
+                    </tr>
+                    <tr>
+                      <td>Residencial Address</td>
+                      <td>{item.patientDetails.address}</td>
+                    </tr>
+                    <tr>
+                      <td>Taluka</td>
+                      <td>{item.patientDetails.talukha}</td>
+                    </tr>
+                    <tr>
+                      <td>Dist</td>
+                      <td>{item.patientDetails.district}</td>
+                    </tr>
+                    <tr>
+                      <td>State</td>
+                      <td>{item.patientDetails.state}</td>
+                    </tr>
+                  </tbody>
+                </table>
+
+                <h1 className="table-heading">Family Details</h1>
+                <table>
+                  <thead>
+                    <tr>
+                      <th>Attribute</th>
+                      <th>Value</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>Family Member</td>
+                      <td>{item.familyDetail[0].name} </td>
+                    </tr>
+                    <tr>
+                      <td>Relation</td>
+                      <td>{item.familyDetail[0].relation}</td>
+                    </tr>
+                    <tr>
+                      <td>Age</td>
+                      <td>{item.familyDetail[0].age}</td>
+                    </tr>
+                    <tr>
+                      <td>Occupation</td>
+                      <td>{item.familyDetail[0].occupation}</td>
+                    </tr>
+                    <tr>
+                      <td>Monthly Income</td>
+                      <td>{item.familyDetail[0].monthlyIncome}</td>
+                    </tr>
+                  </tbody>
+                </table>
+
+                <h1 className="table-heading">Care Taker</h1>
+                <table>
+                  <thead>
+                    <tr>
+                      <th>Attribute</th>
+                      <th>Value</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>Name</td>
+                      <td>{item.careTaker.name} </td>
+                    </tr>
+                    <tr>
+                      <td>Mobile No.1</td>
+                      <td>{item.careTaker.mobile1}</td>
+                    </tr>
+                    <tr>
+                      <td> Mobile No.2</td>
+                      <td>{item.careTaker.mobile2}</td>
+                    </tr>
+                    <tr>
+                      <td>Particulars </td>
+                      <td>{item.careTaker.particulars}</td>
+                    </tr>
+                  </tbody>
+                </table>
+
+                <h1 className="table-heading">Disease Details</h1>
+                <table>
+                  <thead>
+                    <tr>
+                      <th>Attribute</th>
+                      <th>Value</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>Disease Name</td>
+                      <td>{item.diseaseDetail.name}</td>
+                    </tr>
+                    <tr>
+                      <td>Diagnose Date </td>
+                      <td>{item.diseaseDetail.diagnoseDate} </td>
+                    </tr>
+                    <tr>
+                      <td>Diagnose by Dr</td>
+                      <td>{item.diseaseDetail.diagnoseBy}</td>
+                    </tr>
+                    <tr>
+                      <td>Investigation Done</td>
+                      <td>{item.diseaseDetail.investigationDone1}</td>
+                    </tr>
+                    <tr>
+                      <td>Current Hospital Name</td>
+                      <td>{item.diseaseDetail.currentHospitalName}</td>
+                    </tr>
+                    <tr>
+                      <td>Address</td>
+                      <td>{item.diseaseDetail.currentHospitalAddress}</td>
+                    </tr>
+                    <tr>
+                      <td>Contact No.</td>
+                      <td>{item.diseaseDetail.currentHospitalContactNo}</td>
+                    </tr>
+                    <tr>
+                      <td>Current Treatment Details </td>
+                      <td>{item.diseaseDetail.currentTreatmentDetail} </td>
+                    </tr>
+                    <tr>
+                      <td>Doctor’s advice for further process</td>
+                      <td>{item.diseaseDetail.doctorAdviceForFurtherProcess} </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            )}
+
+            {showStatus && (
+              <div className="patient-status-sidebar">
+                <span className="close-icon" onClick={handleSidebarClose}>
+                  ❌
+                </span>
+                <h2>Document</h2>
                 <table>
                   <thead>
                     <tr>
@@ -72,23 +259,19 @@ const RegisteredPatients = () => {
                   <tbody>
                     <tr>
                       <td>ID</td>
-                      <td>{index + 1}</td>
+                      <td></td>
                     </tr>
                     <tr>
                       <td>Name</td>
-                      <td>{item.patientDetails.name}</td>
+                      <td></td>
                     </tr>
                     <tr>
                       <td>Gender</td>
-                      <td>{item.patientDetails.sex}</td>
+                      <td></td>
                     </tr>
-                    {/* <tr>
-                <td>Age</td>
-                <td>{patient.age}</td>
-              </tr> */}
                     <tr>
                       <td>Age</td>
-                      <td>{item.patientDetails.age}</td>
+                      <td></td>
                     </tr>
                     <tr>
                       <td>Document</td>
@@ -119,13 +302,12 @@ const RegisteredPatients = () => {
                     {/* Add other rows for additional details */}
                   </tbody>
                 </table>
-                {/* Fetch additional patient details using Axios or your preferred method */}
-                {/* Display additional details here */}
               </div>
             )}
           </div>
         );
       })}
+     </div>
     </>
   );
 };
