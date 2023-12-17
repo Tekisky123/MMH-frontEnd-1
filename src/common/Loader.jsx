b4a95cc987dfdd53fc0f6f2aea4f673873344dc7
Sulemankhan55 <sullukhan123786@gmail.com>
2023-12-17 19:51:57 +0530

    pull

---
+++ src/common/Loader.jsx
@@ -0,0 +1,31 @@
+// Loader.js
+
+import React, { useState, useEffect } from 'react';
+import "../common/Loader.css"
+
+const Loader = ({ children }) => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate a delay for demonstration purposes
+    const timeout = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
+  return (
+    <>
+      {loading && (
+        <div className="loader">
+          <div className="justify-content-center jimu-primary-loading"></div><br /><br /><br /><br />
+          <h1>loading...</h1>
+        </div>
+      )}
+      {!loading && children}
+    </>
+  );
+};
+
+export default Loader;
